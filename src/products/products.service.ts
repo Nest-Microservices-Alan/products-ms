cd1340311bda884f7e314a968583f6ec54253190
Alan Hernandez <alan.herjuarez97@gmail.com>
2024-09-14 15:08:21 -0600

    added validate products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -93,6 +93,29 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     }
   }
 
+  async validateProducts(ids: number[]) {
+
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids
+        }
+      }
+    });
+    
+    if (products.length !== ids.length) {
+      throw new RpcException({
+        message: 'Some products were not found',
+        status: HttpStatus.BAD_REQUEST
+      })
+    }
+
+    return products;
+
+  }
+
   private handleErrors(e, id) {
     if (e instanceof Prisma.PrismaClientKnownRequestError) {
       if (e.code === 'P2025') {
